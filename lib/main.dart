b3b548d7b71159a5693f88524d27c63363c4b2f5
Francisco Viana <franciscoviana862@gmail.com>
2020-01-12 12:38:47 -0300

    Editado textfield;

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Home(),
-  )); 
+  ));
 }
 
 class Home extends StatefulWidget {
@@ -12,43 +12,74 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _controladorPeso = TextEditingController();
+  final TextEditingController _controladorAltura = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-        centerTitle: true,
-        backgroundColor: _colorFromHex("#1C1C1C"),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      ),
-      backgroundColor: _colorFromHex("#ffffff"),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_outline, size: 120.0, color: _colorFromHex("#363636")),
-          TextField(keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Peso (kg)",
-              labelStyle: TextStyle(color: _colorFromHex("#1C1C1C")),
-              focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: _colorFromHex("#1C1C1C"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
+        backgroundColor: _colorFromHex("#ffffff"),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline,
+                  size: 120.0, color: _colorFromHex("#363636")),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: _colorFromHex("#1C1C1C")),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    border: const OutlineInputBorder(),
+                  ),
+                  //textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: _colorFromHex("#1C1C1C"), fontSize: 25.0),
                 ),
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
               ),
-              
-              border: const OutlineInputBorder(),
-            ),
-            //textAlign: TextAlign.center,
-            style: TextStyle(color: _colorFromHex("#1C1C1C"), fontSize: 25.0),
-          )
-        ],
-      ),
-    );
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: _colorFromHex("#1C1C1C")),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    border: const OutlineInputBorder(),
+                  ),
+                  //textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: _colorFromHex("#1C1C1C"), fontSize: 25.0),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
 
